8fe8cb9d2dcdc6e869daf1e67dd307ce24346be7
Vaud Keith <vaudkeith@gmail.com>
2022-07-03 08:02:47 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers go there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+
+	printf("\n");
+
+	return (0);
+}
