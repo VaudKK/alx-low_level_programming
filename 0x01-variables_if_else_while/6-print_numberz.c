a122b5893d605346e160b502b98c2fa3be69f3b0
Vaud Keith <vaudkeith@gmail.com>
2022-07-03 08:08:47 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+/* more headers go there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+
+	printf("\n");
+
+	return (0);
+}
