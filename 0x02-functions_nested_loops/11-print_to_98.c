9dcd527a41ca9bdefdd03f1ba7e76dffb37e1549
Vaud Keith <vaudkeith@gmail.com>
2022-07-06 10:45:57 -0700

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,82 @@
+#include "main.h"
+
+/**
+ *print_number - handles printing a number to console
+ *@number: the number to be printed
+ */
+void print_number(int number)
+{
+	int ones, tens, hundreds;
+
+	if (number > 99)
+	{
+		hundreds = number / 100;
+		tens = number / 10;
+		ones = number % 10;
+
+		_putchar(hundreds + '0');
+		_putchar(tens + '0');
+		_putchar(ones + '0');
+	}
+	else if (number > 9)
+	{
+		tens = number / 10;
+		ones = number % 10;
+
+		_putchar(tens + '0');
+		_putchar(ones + '0');
+	}
+	else
+	{
+		_putchar(number + '0');
+	}
+
+	if (number != 98)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+}
+
+
+/**
+ * print_to_98 - prints numbers down to or up to 98
+ * @n: number
+ *
+ */
+void print_to_98(int n)
+{
+	int number, absolute;
+
+	number = n;
+
+	if (number > 98)
+	{
+		while (number >= 98)
+		{
+			print_number(number);
+			number--;
+		}
+	}
+	else
+	{
+		while (number <= 98)
+		{
+			if (number < 0)
+			{
+				absolute = number * -1;
+
+				_putchar('-');
+				print_number(absolute);
+			}
+			else
+			{
+				print_number(number);
+			}
+
+			number++;
+		}
+	}
+
+	_putchar('\n');
+}
