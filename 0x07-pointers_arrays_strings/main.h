e682e980f85a21e4178f402ecfeac1931ad015bb
Vaud Keith <vaudkeith@gmail.com>
2022-07-19 14:54:41 -0700

    Taks 6

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,3 +4,5 @@ char *_strchr(char *, char);
 unsigned int _strspn(char *, char *);
 char *_strpbrk(char *, char *);
 char *_strstr(char *, char *);
+int _putchar(char);
+void print_chessboard(char (*a)[8]);
