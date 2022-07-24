761c051b73393066b36788a16d41ab6757896801
Vaud Keith <vaudkeith@gmail.com>
2022-07-24 13:39:04 -0700

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main method
+ * @argc: argument count
+ * @argv: arguments
+ *
+ *
+ * Return: integer
+ */
+int main(int argc, char *argv[])
+{
+	int product = 0;
+	int i, rslt;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (1);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		rslt = atoi(argv[i]);
+
+		if (rslt == 0)
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+
+		product += rslt;
+	}
+
+	printf("%d\n", product);
+
+	return (0);
+}
