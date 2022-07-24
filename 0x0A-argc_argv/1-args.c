a9043fe3f304b8a973cd0b968d9eee3005b5bc6a
Vaud Keith <vaudkeith@gmail.com>
2022-07-24 13:17:27 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main method
+ * @argc: argument count
+ * @argv: arguments
+ *
+ *
+ * Return: integer
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", (argc - 1));
+
+	return (0);
+}
