0fce8df0fa92b149b319d6f9700ee653cae63df2
Vaud Keith <vaudkeith@gmail.com>
2022-07-26 17:49:26 -0700

    Task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+
+/**
+ * alloc_grid - allocate grid
+ * @width: the width
+ * @height: the height
+ *
+ * Return: initialized grid
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i = 0;
+	int j = 0;
+	unsigned int size;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	size = width * height;
+
+	grid = malloc(size * sizeof(int));
+
+	if (grid != NULL)
+	{
+		for (i = 0; i < width; i++)
+		{
+			for (j = 0; j < height; j++)
+			{
+				grid[i * height + j] = 0;
+			}
+		}
+
+		return (grid);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,4 +4,5 @@ int _putchar(char c);
 char *create_array(unsigned int, char);
 char *_strdup(char *);
 char *str_concat(char *, char *);
+int **alloc_grid(int, int)
 #endif
