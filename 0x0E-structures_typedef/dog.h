8eb39b7a392c8084cd96a3cda6229855dd2337a5
Vaud Keith <vaudkeith@gmail.com>
2022-08-01 12:49:38 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_STRUCT
+#define DOG STRUCT
+
+/**
+ * struct dog - dog struct
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+ *
+ * Description: dog descritpion
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
