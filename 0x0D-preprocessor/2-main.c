1d941ef39097e3b3f59218d75f781329ae940999
Vaud Keith <vaudkeith@gmail.com>
2022-08-02 08:42:07 -0700

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - the main method
+ *
+ * Return: 0 success otherwise other numer
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
