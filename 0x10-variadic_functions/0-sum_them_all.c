335652087e99382c786477707481546cbf08d598
Vaud Keith <vaudkeith@gmail.com>
2022-08-04 14:06:19 -0700

    Task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <starg.h>
+
+/**
+ * sum_them_all - sums all the numbers
+ * @n: the number of numbers
+ *
+ * Return: numbers
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list numbers;
+	int i, sum;
+
+	sum = 0;
+
+	va_start(numbers, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(numbers, int);
+
+	va_end(numbers);
+	return (sum);
+}
