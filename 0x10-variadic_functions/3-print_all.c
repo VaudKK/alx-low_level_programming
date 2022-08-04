8a215a9dd8872aa3d4faa1b3c6619f0e907c8646
Vaud Keith <vaudkeith@gmail.com>
2022-08-04 15:55:57 -0700

    Task 4

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,69 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+
+/**
+ * print_string - helper function
+ * @value: the string to print
+ *
+ */
+void print_string(char *value)
+{
+	if (value == NULL)
+	{
+		printf("%s", "(nil)");
+	}
+	else
+	{
+		printf("%s", value);
+	}
+}
+
+/**
+ * print_all - prints all characters
+ * @format: the format of the characters
+ *
+ *
+ */
+void print_all(const char * const format, ...)
+{
+	va_list values;
+	int i = 0;
+	char *forstrings;
+	int valid = 0;
+
+	va_start(values, format);
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(values, int));
+				valid = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(values, int));
+				valid = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(values, double));
+				valid = 1;
+				break;
+			case 's':
+				forstrings = va_arg(values, char *);
+				print_string(forstrings);
+				valid = 1;
+				break;
+			default:
+				valid = 0;
+				break;
+		}
+
+		if (valid == 1 && format[i + 1] != '\0')
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
+}
