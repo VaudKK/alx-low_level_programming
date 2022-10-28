cfa62fb9207dc66e0586aff49a482dea307c479a
Vaud Keith <Vaudkeith@gmail.com>
2022-10-28 09:36:15 -0700

    Task 3 restoring external linear chaiing

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,6 +28,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->key = (char *)key;
 	node->value = (char *)value;
 
+	if (table[index] != NULL)
+	{
+		node->next = table[index];
+	}
+
 	table[index] = node;
 	return (1);
 }
