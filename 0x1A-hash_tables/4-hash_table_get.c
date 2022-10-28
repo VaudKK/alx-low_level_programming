6d751491ded04e91a0a5d18a4c9a439e17f718fd
Vaud Keith <Vaudkeith@gmail.com>
2022-10-28 09:47:05 -0700

    Task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * has_table_get - get value
+ * hash_table_get - get value
  *
  * @ht: the hashtable
  * @key: the associated key
@@ -10,5 +10,19 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int key = key_index(key, ht->size);
+	unsigned long int key;
+	hash_node_t **array;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	key = key_index(key, ht->size);
+	array = ht->array;
+
+	if (array[key] != NULL)
+	{
+		return (array[key]->value);
+	}
+
+	return (NULL);
 }
