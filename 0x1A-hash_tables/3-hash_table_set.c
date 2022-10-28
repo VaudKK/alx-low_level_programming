13b2745be18584a1e2661c8089483b3578ded34b
Vaud Keith <Vaudkeith@gmail.com>
2022-10-28 09:28:08 -0700

    Task 3 malloc handling

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node = malloc(sizeof(hash_node_t));
+	hash_node_t *node;
 	unsigned long int index;
 	hash_node_t **table;
 
@@ -21,6 +21,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((unsigned char *)key, ht->size);
 	table = ht->array;
 
+	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
 
