47204f60ff901ad222dfcc14c652344287603b06
Vaud Keith <Vaudkeith@gmail.com>
2022-10-28 09:33:24 -0700

    Task 3 removing external linear chaining

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,11 +28,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->key = (char *)key;
 	node->value = (char *)value;
 
-	if (table[index] != NULL)
-	{
-		node->next = table[index];
-	}
-
 	table[index] = node;
 	return (1);
 }
