4068bfb4aa00b12b0934fcec295945086739e199
Vaud Keith <Vaudkeith@gmail.com>
2022-10-29 12:01:37 -0700

    Task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -3,32 +3,38 @@
 
 
 /**
- *
  * hash_table_delete - delete hash table
  * @ht: the hash table to delete
  *
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *temp;
-	hash_node_t **arr;
+	hash_node_t **array, *temp, *to_delete;
 	unsigned long int i;
 
-	if (ht != NULL)
-	{
-		arr = ht->array;
-		
-		for (i = 0; i < ht->size; i++)
-		{
-			if (arr[i] != NULL)
-			{
-				temp = arr[i];
-				if (temp->next != NULL)
-				{
+	if (ht == NULL)
+		return;
 
-				}
-				free(temp);
+	array = ht->array;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = array[i];
+		if (temp != NULL && temp->next != NULL)
+		{
+			temp = temp->next;
+
+			while (temp != NULL)
+			{
+				to_delete = temp;
+				temp = temp->next;
+				free(to_delete);
 			}
 		}
+		if (array[i] != NULL)
+			free(array[i]);
 	}
+
+	free(array);
+	free(ht);
 }
