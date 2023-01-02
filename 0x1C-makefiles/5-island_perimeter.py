5e83cf081d0662018dfc743f659ae51c2bcfd523
VaudKK <vaudkeith@gmail.com>
2023-01-02 19:12:39 +0300

    Task 5 fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,27 +4,21 @@
 island perimeter module
 """
 
+
 def island_perimeter(grid):
     """
     calculates the island perimeter
     """
     deductions = 0
     assumed_perimeter = 0
-    for row in grid:
-        for col in row:
-            if col == 1:
-                assumed_perimeter += 4
 
-    for grid_index,row in enumerate(grid):
+    for grid_index, row in enumerate(grid):
         for row_index, col in enumerate(row):
             if col == 1:
-                try:
-                    if row[row_index -1] == 1:
-                        deductions += 1
-                    if grid[grid_index-1][row_index] == 1:
-                        deductions += 1
-                except:
-                    pass
-
+                assumed_perimeter += 4
+                
+                if grid_index != 0 and grid[grid_index-1][row_index] == 1:
+                    deductions += 1
+                if row_index != 0 and row[row_index-1] == 1:
+                    deductions += 1
     return assumed_perimeter - (deductions * 2)
-
